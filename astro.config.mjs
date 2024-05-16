d3d71ea66489e2f05830db401a2b83bec9481c37
John Mata <john.mata@epn.edu.ec>
2024-05-16 03:59:36 -0500

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://JohnMata0427.github.io/Prueba-01-Parte-Practica.git',
-  base: 'https://github.com/JohnMata0427/Prueba-01-Parte-Practica.git',
+  site: 'https://johnmata0427.github.io/',
 })
