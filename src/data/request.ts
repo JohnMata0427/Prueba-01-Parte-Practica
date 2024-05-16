4645e6938da81ecdcecb33458ff6719bc8f1daff
John Mata <john.mata@epn.edu.ec>
2024-05-16 03:40:40 -0500

    change API KEY

--- src/data/request.ts
+++ src/data/request.ts
@@ -2,7 +2,7 @@ import { type MoviesTypes, type MovieListTypes } from './types';
 
 export async function requestAPI (id: number) {
     try {
-      const response = await fetch(`https://api.watchmode.com/v1/title/${id}/details?apiKey=4Vf0qlVm4SszUIlyS6tN2h4NaBFXUbCDthvrdkBF&append_to_response=sources`);
+      const response = await fetch(`https://api.watchmode.com/v1/title/${id}/details?apiKey=XmbTNXn9eKOcJ8yjZESOfqMVLh7XwXK35Pt44T9k&append_to_response=sources`);
       const data = (await response.json() as MoviesTypes);
       return data;
     } catch (error) {
@@ -12,7 +12,7 @@ export async function requestAPI (id: number) {
 
 export async function requestAPIList () {
     try {
-      const response = await fetch('https://api.watchmode.com/v1/list-titles/?apiKey=4Vf0qlVm4SszUIlyS6tN2h4NaBFXUbCDthvrdkBF&append_to_response=sources');
+      const response = await fetch('https://api.watchmode.com/v1/list-titles/?apiKey=XmbTNXn9eKOcJ8yjZESOfqMVLh7XwXK35Pt44T9k&append_to_response=sources');
       const data = (await response.json() as MovieListTypes);
       return data;
     } catch (error) {
